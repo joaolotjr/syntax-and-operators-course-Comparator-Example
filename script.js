da10a9756679ad78737999ebf93f7d78a68306ab
joaolotjr <joaolotjr@gmail.com>
2022-05-28 23:56:03 -0300

    starting

---
+++ script.js
@@ -0,0 +1,10 @@
+function compareNumber(num1, num2) {
+  const equals = num1 === num2
+  const sum = num1 + num2
+
+//   if (equals) {
+//       return "They are equals"
+//   }
+//   return "They are not equals"
+// }
+return equals ? "They are equals" : "They are not equals";
